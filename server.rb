dc90cfca4e284dccb60a6e62a880ffb2b5113e6d
David FletcherE <dtsfletcher@gmail.com>
2017-08-23 16:14:29 -0400

    DONE

--- server.rb
+++ server.rb
@@ -10,5 +10,16 @@ erb :gallery
 end
 
 get '/about' do
+@skills = ['git', 'HTML', 'CSS', 'Ruby']
+@interests = ['cats', 'art', 'music', 'films', 'coffee']
 erb :about_me
+end
+
+get '/favourites' do
+@fav_links = ["www.bbc.com", "www.hotmail.com", "www.cbc.ca"]
+erb :favourites
+end
+
+get '/' do
+  redirect to('/home')
 end
